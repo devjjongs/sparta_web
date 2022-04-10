99581e2682ad03558b7c0789d6693ee833bc9573
devjjongs <whdtjr5689@gmail.com>
2022-04-10 14:12:02 +0900

    practice crawling
    pymongo와 mongoDB를 이용해 크롤링한 네이버 영화 랭킹 정보를 mongoDB에 저장하는 코드 연습

--- pythonprac/hello2.py
+++ pythonprac/hello2.py
@@ -1,28 +1,10 @@
-# python 실행 단축 Ctrl + Shift + F10
-print('hello sparta!!')
+# pymongoDB 사용을 위한 명령
+from pymongo import MongoClient
 
-# 가상 환경(virtual environment)이란? - 프로젝트별로 패키지들을 담을 공구함
-# 	같은 시스템에서 실행되는 다른 파이썬 응용 프로그램들의 동작에 영향을 주지 않기 위해,
-# 	파이썬 배포 패키지들을 설치하거나 업그레이드 하는 것을 가능하게 하는 격리된 싱행 환경
-
-
-# import requests  # requests 라이브러리 설치 필요
-#
-# r = requests.get('http://openapi.seoul.go.kr:8088/6d4d776b466c656533356a4b4b5872/json/RealtimeCityAir/1/99')
-# rjson = r.json()
-#
-# gus = rjson['RealtimeCityAir']['row']
-#
-# for gu in gus:
-# 	gu_name = gu['MSRSTE_NM']
-# 	gu_mise = gu['IDEX_MVL']
-#
-# 	if (gu_mise > 100):
-# 		print(gu_name, gu_mise)
-#
-# import requests
-# from bs4 import BeautifulSoup
-
+# 로컬에서 돌아가는 mongoDB에 연결
+client = MongoClient('localhost', 27017)
+# dbsparta 이름을 가진 DB에 접속
+db = client.dbsparta
 
 # 웹스크래핑(크롤링 기초)
 import requests  # 요청 명령 불러오기
@@ -53,22 +35,30 @@ trs = soup.select('#old_content > table > tbody > tr')
 for tr in trs:
 	# print(tr)
 	
-	# 영화 랭킹 순위값 불러오기
-	# old_content > table > tbody > tr:nth-child(2) >
-	rank = tr.select_one('td:nth-child(1) > img')
-	
 	# 영화 이름
 	# old_content > table > tbody > tr:nth-child(2) >
 	a_tag = tr.select_one('td.title > div > a')
 	# print(a_tag)
-	
-	# 영화 평점
-	# old_content > table > tbody > tr:nth-child(2) >
-	rate = tr.select_one('td.point')
 	
 	if a_tag is not None:
 		title = a_tag.text
 		# print(rank['alt'])
 		# print(title)
 		# print(rate.text)
-		print(lank['alt'], title, rate.text)
+		# print(lank['alt'], title, rate.text)
+		
+		# 영화 랭킹 순위값 불러오기
+		# old_content > table > tbody > tr:nth-child(2) >
+		rank = tr.select_one('td:nth-child(1) > img')['alt']
+		
+		# 영화 평점
+		# old_content > table > tbody > tr:nth-child(2) >
+		rate = tr.select_one('td.point').text
+		
+		# DB에 크롤링한 영화 data 저장하기
+		doc = {
+			'rank': rank,
+			'title': title,
+			'star': rate
+		}
+		db.movies.insert_one(doc)
