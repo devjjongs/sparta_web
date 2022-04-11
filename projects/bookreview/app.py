fb89ff7eeb5ec709fd195656a5bfd7ab348a892d
devjjongs <whdtjr5689@gmail.com>
2022-04-11 14:51:36 +0900

    practice bookreview GET/POST
    -GET API를 이용하여 모두의 책 리뷰 프로젝트의 입력 값을 데이터 베이스에 저장
    -POST API를 이용하여 데이터 베이스에 저장되어 있는 모두의 책 리뷰 데이터 값을 호출

--- projects/bookreview/app.py
+++ projects/bookreview/app.py
@@ -1,28 +1,44 @@
 from flask import Flask, render_template, jsonify, request
+
 app = Flask(__name__)
 
 from pymongo import MongoClient
+
 client = MongoClient('localhost', 27017)
 db = client.dbsparta
 
+
 ## HTML을 주는 부분
 @app.route('/')
-def home():
+def home ():
     return render_template('index.html')
+
 
 ## API 역할을 하는 부분
 @app.route('/review', methods=['POST'])
-def write_review():
-    sample_receive = request.form['sample_give']
-    print(sample_receive)
-    return jsonify({'msg': '이 요청은 POST!'})
+def write_review ():
+    title_receive = request.form['title_give']
+    author_receive = request.form['author_give']
+    review_receive = request.form['review_give']
+    # print(sample_receive)
+
+    # doc 목록 작성
+    doc = {
+        'title': title_receive,
+        'author': author_receive,
+        'review': review_receive
+    }
+
+    # DB 저장
+    db.bookreview.insert_one(doc)
+
+    return jsonify({'msg': '저장 완료!'})
 
 
 @app.route('/review', methods=['GET'])
-def read_reviews():
-    sample_receive = request.args.get('sample_give')
-    print(sample_receive)
-    return jsonify({'msg': '이 요청은 GET!'})
+def read_reviews ():
+    reviews = list(db.bookreview.find({}, {'_id': False}))
+    return jsonify({'all_reviews': reviews})
 
 
 if __name__ == '__main__':
